362835f2c0da3e55d12882031df30dfabd6fb110
shin-sakata <shintaro.sakata.tokyo@gmail.com>
2024-01-04 10:54:06 +0900

    install db

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,11 +1,27 @@
 import { publicProcedure, router } from "./trpc";
 import { z } from "zod";
+import { prisma } from "@packages/db";
 
 export const appRouter = router({
   greeting: publicProcedure
     .input(z.object({ name: z.string() }))
     .query(({ input: { name } }) => {
       return `Hello ${name}!`;
+    }),
+  getPosts: publicProcedure.query(async () => {
+    const posts = await prisma.posts.findMany();
+    return posts;
+  }),
+  createPost: publicProcedure
+    .input(z.object({ title: z.string(), content: z.string() }))
+    .mutation(async ({ input: { title, content } }) => {
+      const post = await prisma.posts.create({
+        data: {
+          title,
+          content,
+        },
+      });
+      return post;
     }),
 });
 
